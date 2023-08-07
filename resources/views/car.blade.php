319f292a39b43f56aee05e80b202ee0856f739ff
BryanChew <bryanchew001@gmail.com>
2023-08-07 17:02:51 +0800

    add cars database, car model, car page and car route

---
+++ resources/views/car.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Car Rental List</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container mt-5">
+            <h2 class="mb-3">Car Rental List</h2>
+            <div class="card">
+                <div class="card-body">
+                    <table id="carList" class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Car Plate No.</th>
+                                <th>Colour</th>
+                                <th>Propellant</th>
+                                <th>Seats</th>
+                                <th>Expiry Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(function(){
+            var table = $('#carList').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('car.index') }}",
+                columns: [
+                    {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                    {data: 'carPlate', name: 'carPlate'},
+                    {data: 'colour', name: 'colour'},
+                    {data: 'propellant', name: 'propellant'},
+                    {data: 'seats', name: 'seats'},
+                    {data: 'expiryDate', name: 'expiryDate'},
+                    {data: 'action', name: 'action', orderable: false, searchable: false},
+                ]
+            });
+        });
+    </script>
+</html>
